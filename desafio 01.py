42287b7f8962aeff202013d6763be7cd134ad2a8
23diegob <diegob231207@gmail.com>
2026-03-16 22:45:42 -0300

    aula 02 de python

--- desafio 01.py
+++ desafio 01.py
@@ -1,6 +1,8 @@
 nome = input('Digite seu nome:')
 print('seja bem vindo(a),', nome, "!")
 
-dia = input ('Digite o seu n')
-mês = input ('Digite o seu mês')
-ano = input ('Digite o seu x')
+dia = input ('Digite o seu dia de nascimento:')
+mês = input ('Digite o seu mês de nascimento:')
+ano = input ('Digite o seu ano de nascimento:')
+
+print (dia, mês, ano)
