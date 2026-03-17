78758cdf1d60315251ad75e789868cf7b1989186
23diegob <diegob231207@gmail.com>
2026-03-16 22:41:01 -0300

    aula 02 de python

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,28 @@
+print("olá mundo")
+
+print (7+4)
+print("7 + 4")
+print("7" + "4")
+
+# Comentários de 1 linha no python
+
+'''
+   Comentários de 
+   múltiplas 
+   linhas
+'''
+
+# VARIÁVEIS
+
+nome = "Diego"
+idade = 18 # int
+peso = 83.0 # str
+
+print(nome, idade, peso)
+
+#INPUT - SIMULAR UM FORMULÁRIO NO CMD
+nome = input("Digite seu nome:") #str
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
---
+++ desafio 01.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome:')
+print('seja bem vindo(a),', nome, "!")
+
+dia = input ('Digite o seu n')
+mês = input ('Digite o seu mês')
+ano = input ('Digite o seu x')
